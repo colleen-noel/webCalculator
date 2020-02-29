64c973227df4ccfd758dc3fbfdce9e4d4924ab90
Colleen Noel <noel.colleen@gmail.com>
2020-02-29 14:54:42 -0500

    added other operations to calculator

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-function sum() {
-    let a = document.getElementById("firstNumber");
-    let b = document.getElementById("secondNumber");
-    let aNum = parseInt(a.value);
-    let bNum = parseInt(b.value);
+function calculate() {
+    var operationDropDown = document.getElementById("operation")
+    var firstNumElement = document.getElementById("firstNumber")
+    var secondNumElement = document.getElementById("secondNumber")
+    var firstNumber = parseInt(firstNumElement.value)
+    var secondNumber = parseInt(secondNumElement.value)
 
-    let sum = aNum + bNum;
-    alert("Your answer is " + sum);
-    a.value = "";
-    b.value = "";
 
+
+    if (operationDropDown.value === "addition") {
+        var sum = firstNumber + secondNumber
+        alert("Your answer is " + sum)
+        firstNumElement.value = ""
+        secondNumElement.value = ""
+    }
+    else if (operationDropDown.value === "subtraction") {
+        var difference = firstNumber - secondNumber
+        alert("Your answer is " + difference)
+        firstNumElement.value = ""
+        secondNumElement.value = ""
+    }
+    else if (operationDropDown.value === "multiplication") {
+        var product = firstNumber * secondNumber
+        alert("Your answer is " + product)
+        firstNumElement.vaue = ""
+        secondNumElement.value = ""
+    }
+    else if (operationDropDown.value === "division") {
+        var quotient = firstNumber / secondNumber
+        alert("Your answer is " + quotient)
+        firstNumElement.value = ""
+        secondNumElement.value = ""
+    }
+    else {
+        alert("Please choose an operation.")
+    }
 }
+
+
